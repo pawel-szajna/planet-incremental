9dbb8596bc925ddb85c87243b7f18c45d3ed995f
szajna <pawel.1.szajna@nokia.com>
2023-03-31 21:27:12 +0200

    resources: tracked value first functionality

--- src/resources/include/Tracked.hpp
+++ src/resources/include/Tracked.hpp
@@ -2,4 +2,79 @@
 
 template<typename T>
 class Tracked
-{};
+{
+public:
+
+    Tracked() : value{} {}
+    explicit Tracked(T value) : value{value} {}
+
+    T operator*() const
+    {
+        return value;
+    }
+
+    bool operator==(const T& other) const
+    {
+        return value == other;
+    }
+
+    bool operator==(const Tracked<T>& other) const
+    {
+        return value == other.value;
+    }
+
+    Tracked& operator=(T other)
+    {
+        value = other;
+        return *this;
+    }
+
+    Tracked operator+(T other) const
+    {
+        return Tracked(value + other);
+    }
+
+    Tracked operator+(const Tracked<T>& other) const
+    {
+        return operator+(other.value);
+    }
+
+    Tracked operator-(T other) const
+    {
+        return Tracked(value - other);
+    }
+
+    Tracked operator-(const Tracked<T>& other) const
+    {
+        return operator-(other.value);
+    }
+
+    Tracked operator*(T other) const
+    {
+        return Tracked(value * other);
+    }
+
+    Tracked operator*(const Tracked<T>& other) const
+    {
+        return operator*(other.value);
+    }
+
+    Tracked operator/(T other) const
+    {
+        return Tracked(value / other);
+    }
+
+    Tracked operator/(const Tracked<T>& other) const
+    {
+        return operator/(other.value);
+    }
+
+    explicit operator T() const
+    {
+        return value;
+    }
+
+private:
+
+    T value;
+};
--- src/resources/tests/TrackedTests.cpp
+++ src/resources/tests/TrackedTests.cpp
@@ -2,7 +2,84 @@
 
 #include <gtest/gtest.h>
 
-TEST(TrackedTests, FirstDummyUT)
+using namespace ::testing;
+
+template<typename T>
+class TrackedBasicTests : public Test
 {
-    ASSERT_EQ(true, true);
+protected:
+    template<typename... Args>
+    auto make_sut(Args&&... args)
+    {
+        return Tracked<T>(std::forward<Args>(args)...);
+    }
+};
+
+using TrackedBasicTestsTypes = Types<int, unsigned, double>;
+TYPED_TEST_SUITE(TrackedBasicTests, TrackedBasicTestsTypes);
+
+TYPED_TEST(TrackedBasicTests,
+           shouldCarryTypeDefaultValueOnConstruction)
+{
+    auto sut = this->make_sut();
+    ASSERT_EQ(*sut, TypeParam{});
+    ASSERT_TRUE(sut == TypeParam{});
+}
+
+TYPED_TEST(TrackedBasicTests,
+           shouldAssignAndReturnValueOfUnderlyingType)
+{
+    auto sut = this->make_sut(4);
+    ASSERT_EQ(*sut, TypeParam{4});
+    ASSERT_TRUE(sut == TypeParam{4});
+
+    sut = TypeParam{5};
+    ASSERT_NE(*sut, TypeParam{4});
+    ASSERT_FALSE(sut == TypeParam{4});
+    ASSERT_EQ(*sut, TypeParam{5});
+    ASSERT_TRUE(sut == TypeParam{5});
+
+    TypeParam value{};
+    value = (TypeParam)sut;
+    ASSERT_EQ(value, TypeParam{5});
+}
+
+TYPED_TEST(TrackedBasicTests,
+           shouldCompareMultipleTrackedValues)
+{
+    auto sut1 = this->make_sut(2);
+    auto sut2 = this->make_sut(3);
+    auto sut3 = this->make_sut(3);
+
+    ASSERT_NE(sut1, sut2);
+    ASSERT_NE(sut1, sut3);
+    ASSERT_EQ(sut2, sut3);
+
+    sut3 = sut1;
+
+    ASSERT_NE(sut1, sut2);
+    ASSERT_EQ(sut1, sut3);
+    ASSERT_NE(sut2, sut3);
+}
+
+TYPED_TEST(TrackedBasicTests,
+           shouldPerformBasicOperations)
+{
+    TypeParam valueA = 2;
+    TypeParam valueB = 3;
+    auto sutA = this->make_sut(valueA);
+    auto sutB = this->make_sut(valueB);
+
+    ASSERT_EQ(valueA + valueB, sutA + sutB);
+    ASSERT_EQ(valueA + valueB, sutB + sutA);
+    ASSERT_EQ(valueA + valueB, sutA + valueB);
+
+    ASSERT_EQ(valueB - valueA, sutB - sutA);
+    ASSERT_EQ(valueB - valueA, sutB - valueA);
+
+    ASSERT_EQ(valueA * valueB, sutA * sutB);
+    ASSERT_EQ(valueA * valueB, sutA * valueB);
+
+    ASSERT_EQ(valueA / valueB, sutA / sutB);
+    ASSERT_EQ(valueA / valueB, sutA / valueB);
 }
