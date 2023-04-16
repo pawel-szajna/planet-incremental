951c2943c2e5997b2ef8db5ae0b160c26d0631ac
Pawel Szajna <szajnapawel@gmail.com>
2023-04-16 17:35:01 +0200

    resources: fix Elements class on gcc

--- src/resources/include/resources/Elements.hpp
+++ src/resources/include/resources/Elements.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <string>
-#include <utility>
+#include <tuple>
 #include <vector>
 
 namespace Resources
--- src/resources/src/Elements.cpp
+++ src/resources/src/Elements.cpp
@@ -1,5 +1,7 @@
 #include "Elements.hpp"
 
+#include <algorithm>
+
 namespace Resources
 {
 Elements::Elements()
